aeaa07d8f4b88c007ede697228e67d0a7a62a723
Avinash Kumar <69908168+know4avi@users.noreply.github.com>
2022-03-08 17:41:02 +0530

    Modified lab2 ex4

--- src/lab2/Ex4.java
+++ src/lab2/Ex4.java
@@ -7,83 +7,81 @@ import java.util.*;
 // Return the resulting array in descending order
 
 public class Ex4 {
-	
-	public static void reverse(int[] array)
-    {
- 
-        int n = array.length;
-     
-        for (int i = 0; i < n / 2; i++) {
-  
-            int temp = array[i];
-  
-            array[i] = array[n - i - 1];
 
-            array[n - i - 1] = temp;
-        }
-    }
-	
+	public static void reverse(int[] array) {
+
+		int n = array.length;
+
+		for (int i = 0; i < n / 2; i++) {
+
+			int temp = array[i];
+
+			array[i] = array[n - i - 1];
+
+			array[n - i - 1] = temp;
+		}
+	}
+
 	// if the array is already sorted
 	static int[] modifyArray(int[] a) {
-		
+
 		int n = a.length;
-		
+
 		if (n == 0 || n == 1) {
-            return a;
-        }
-  
-        int newLength = 0;
-  
-        for (int i = 0; i < n - 1; i++) {
-            if (a[i] != a[i + 1]) {
-                a[newLength++] = a[i];
-            }
-        }
-  
-        a[newLength++] = a[n - 1];
-        
-        int[] ans = new int[newLength];
-        
-        for (int i = 0; i < ans.length; i++) {
+			return a;
+		}
+
+		int newLength = 0;
+
+		for (int i = 0; i < n - 1; i++) {
+			if (a[i] != a[i + 1]) {
+				a[newLength++] = a[i];
+			}
+		}
+
+		a[newLength++] = a[n - 1];
+
+		int[] ans = new int[newLength];
+
+		for (int i = 0; i < ans.length; i++) {
 			ans[i] = a[i];
 		}
-        
-        Arrays.sort(ans);
-        reverse(ans);
-  
-        return ans;
+
+		Arrays.sort(ans);
+		reverse(ans);
+
+		return ans;
 	}
-	
+
 	// if the array is not already sorted,
 	// then use set collection
 	static void modifyArray2(int[] a) {
-		
-	   LinkedHashSet<Integer> arrSet = new LinkedHashSet<Integer>();
 
-	   for (int i = 0; i < a.length; i++)
-		   arrSet.add(a[i]);
-	   
-	   ArrayList<Integer> arr = new ArrayList<>(arrSet);
-	   
-	   Collections.sort(arr, Collections.reverseOrder());
-	   
-	   System.out.print(arr);
+		LinkedHashSet<Integer> arrSet = new LinkedHashSet<Integer>();
+
+		for (int i = 0; i < a.length; i++)
+			arrSet.add(a[i]);
+
+		ArrayList<Integer> arr = new ArrayList<>(arrSet);
+
+		Collections.sort(arr, Collections.reverseOrder());
+
+		System.out.print(arr);
 
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
-		int[] arr1 = {2, 2, 4, 5, 5, 9 ,10 ,10};
-		
-		int[] arr2 = {22, 22, 4, 15, 15, 99 ,10 ,10};
-		
+
+		int[] arr1 = { 2, 2, 4, 5, 5, 9, 10, 10 };
+
+		int[] arr2 = { 22, 22, 4, 15, 15, 99, 10, 10 };
+
 		System.out.println(Arrays.toString(modifyArray(arr1)));
-		
+
 		System.out.println();
-		
+
 		modifyArray2(arr2);
-		
+
 	}
 
 }
